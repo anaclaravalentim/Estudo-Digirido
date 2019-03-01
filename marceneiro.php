4f741259add2a0480ef31167a4576c7734297a22
anaclaravalentim <48128230+anaclaravalentim@users.noreply.github.com>
2019-03-01 11:55:14 -0300

    Add files via upload

---
+++ marceneiro.php
@@ -0,0 +1,13 @@
+<?php
+ 
+ $tabua1 = 300;
+ $tabua2 = 400;
+ $tabua3 = 500;
+
+    $metro1 =  300/45;
+    $metro2 =  400/45;
+    $metro3 =  500/45;
+
+print "De 3 metros foi possivel cortar: $metro1 cm \n";
+print "De 4 metros foi possivel cortar: $metro2 cm \n";
+print "De 5 metros foi possivel cortar: $metro3 cm \n";
